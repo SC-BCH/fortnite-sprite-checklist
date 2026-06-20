1df393de98de3c4dea7b888b8ce6e78d60498633
BCH_ServOfMiori <163378672+SC-BCH@users.noreply.github.com>
2026-06-20 11:55:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,8 @@ const ASSETS = [
   "./index.html",
   "./manifest.webmanifest",
   "./sw.js",
-  "./icons/icon.svg"
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
